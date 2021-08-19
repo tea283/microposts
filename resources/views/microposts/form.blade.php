fadad63f46e5fe4593a806b9d52f45014d924a07
Ubuntu <ubuntu@ip-172-31-23-71.ap-northeast-1.compute.internal>
2021-08-19 07:30:54 +0000

    post

---
+++ resources/views/microposts/form.blade.php
@@ -0,0 +1,13 @@
+{!! Form::open(['route' => 'microposts.store']) !!}
+    <div class="form-group">
+        {!! Form::textarea('content', null, ['class' => 'form-control', 'rows' => '2']) !!}
+        {!! Form::submit('Post', ['class' => 'btn btn-primary btn-block']) !!}
+    </div>
+{!! Form::close() !!}
+{{-- ユーザ詳細タブ --}}
+    <li class="nav-item">
+        <a href="{{ route('users.show', ['user' => $user->id]) }}" class="nav-link {{ Request::routeIs('users.show') ? 'active' : '' }}">
+            TimeLine
+            <span class="badge badge-secondary">{{ $user->microposts_count }}</span>
+        </a>
+    </li>
